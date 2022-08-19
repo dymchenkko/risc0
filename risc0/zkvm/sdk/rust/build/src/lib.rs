5378e4f3e2e4caef70637f8c22cabf65be033bbf
dycmhenkko <elizavetadymchenko20@gmail.com>
2022-08-19 09:44:41 +0200

    CARGO_TARGET_RISCV32IM_RISC0_ZKVM_ELF_RUNNER as a string

--- risc0/zkvm/sdk/rust/build/src/lib.rs
+++ risc0/zkvm/sdk/rust/build/src/lib.rs
@@ -422,7 +422,7 @@ fn test_guest_package<P>(
         .env("__CARGO_TESTS_ONLY_SRC_ROOT", risc0_standard_lib)
         .env(
             "CARGO_TARGET_RISCV32IM_RISC0_ZKVM_ELF_RUNNER",
-            "[\"target/build/r0vm\"]",
+            "/bin/true --elf",
         )
         .args(args)
         .stderr(Stdio::piped())
