9cb0d1edc2faa3aba1564c9a1a7a679eeda32718
dycmhenkko <elizavetadymchenko20@gmail.com>
2022-08-22 16:46:08 +0200

    add parameters to the runner

--- risc0/zkvm/sdk/rust/build/src/lib.rs
+++ risc0/zkvm/sdk/rust/build/src/lib.rs
@@ -25,7 +25,6 @@ use std::{
     process::{Command, Stdio},
 };
 
-use assert_fs::{fixture::PathChild, TempDir};
 use cargo_metadata::{MetadataCommand, Package};
 use risc0_zkvm::host::{MethodId, DEFAULT_METHOD_ID_LIMIT};
 use risc0_zkvm_platform_sys::LINKER_SCRIPT;
@@ -416,10 +415,6 @@ fn test_guest_package<P>(
     };
 
     println!("Using rust standard library root: {}", risc0_standard_lib);
-
-    let temp = TempDir::new().unwrap();
-    let receipt_file = temp.child("receipt.dat");
-    let method_id_file = temp.child("method_id.dat");
 
     let mut cmd = Command::new(cargo);
     let mut child = cmd
