af7b61f04da77e5a7250c6fba24e0fef77ba61fd
dycmhenkko <elizavetadymchenko20@gmail.com>
2022-08-24 12:08:08 +0200

    target_dir to string

--- risc0/zkvm/sdk/rust/build/src/lib.rs
+++ risc0/zkvm/sdk/rust/build/src/lib.rs
@@ -402,9 +402,10 @@ fn test_guest_package<P>(
         "--target-dir",
         target_dir.as_ref().to_str().unwrap(),
     ];
-    let root = "--root=".to_owned()
+    let root = ("--root=".to_owned()
         + &target_dir.as_ref().to_str().unwrap().to_owned()
-        + "/r0vm risc0-r0vm";
+        + "/r0vm risc0-r0vm")
+        .as_str();
     let mut args2 = vec!["install", root];
     let features_str = features.join(",");
     if !features.is_empty() {
